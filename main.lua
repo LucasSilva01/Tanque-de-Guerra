b6631b0d6e4b0e43aa78e362a5d4e53a0f38fd10
LucasSilva01 <53836194+LucasSilva01@users.noreply.github.com>
2022-12-23 20:14:56 -0300

    função de atirar e organização

--- main.lua
+++ main.lua
@@ -4,6 +4,12 @@ alturaTela = love.graphics.getHeight()
 function love.load()
     imgTank = love.graphics.newImage( "imagens/tank.png" )
     love.mouse.setVisible( false )
+    atira = true
+    delayT = 0.1
+    timeTiro = delayT
+    tiros = {}
+    imgTiro = love.graphics.newImage("imagens/bala.png")
+    
 
     tank = {
         posX = larguraTela / 2,
@@ -14,6 +20,31 @@ function love.load()
 
 function love.update(dt)
     move(dt)
+    atirar(dt)
+end
+
+function atirar(dt)
+    
+    timeTiro = timeTiro - (1 * dt)
+
+    if timeTiro < 0 then 
+        atira = true
+    end
+
+    if love.keyboard.isDown("space") and atira then
+        newtiro = {x = tank.posX, y = tank.posY, img = imgTiro}
+        table.insert(tiros, newtiro)
+        atira = false
+        timeTiro =  delayT
+    end
+
+    for i, tiro in ipairs(tiros) do
+        tiro.y = tiro.y - (500 * dt)
+        if tiro.y < 0 then
+            table.remove(tiros, i)
+        end
+    end
+
 end
 
 function move(dt)
@@ -41,6 +72,9 @@ function move(dt)
 
 end
 function love.draw()
-    rot = angulo
     love.graphics.draw(imgTank, tank.posX, tank.posY, 0, 1, 1, imgTank:getWidth()/2, imgTank:getHeight()/2)
+    
+    for i, tiro in ipairs(tiros) do
+        love.graphics.draw(tiro.img, tiro.x, tiro.y, 0, 1, 1, imgTiro:getWidth()/2, imgTiro:getHeight()/2)
+    end
 end
