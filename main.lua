21d14b9aeee09ca63a4096882fdd13b69275ee29
LucasSilva01 <53836194+LucasSilva01@users.noreply.github.com>
2022-12-23 19:54:27 -0300

    pequena correção de bug

--- main.lua
+++ main.lua
@@ -2,10 +2,10 @@ larguraTela = love.graphics.getWidth()
 alturaTela = love.graphics.getHeight()
 
 function love.load()
-    imgTank = love.graphics.newImage( "imagens/game.png" )
+    imgTank = love.graphics.newImage( "imagens/tank.png" )
     love.mouse.setVisible( false )
 
-    nave = {
+    tank = {
         posX = larguraTela / 2,
         posY = alturaTela / 2,
         velocidade = 200
@@ -13,32 +13,34 @@ function love.load()
     end
 
 function love.update(dt)
+    move(dt)
+end
+
+function move(dt)
     if love.keyboard.isDown("left") then
-        if nave.posX > (0 + imgTank:getWidth()/2)then
-            nave.posX = nave.posX - nave.velocidade * dt
+        if tank.posX > (0 + imgTank:getWidth()/2)then
+            tank.posX = tank.posX - tank.velocidade * dt
         end
         
     end
     
     if love.keyboard.isDown("right") then
-        if nave.posX < (larguraTela - imgTank:getWidth()/2) then
-            nave.posX = nave.posX + nave.velocidade * dt
+        if tank.posX < (larguraTela - imgTank:getWidth()/2) then
+            tank.posX = tank.posX + tank.velocidade * dt
         end 
     end
 
     if love.keyboard.isDown("up") then
-        nave.posY = nave.posY - nave.velocidade * dt
+        tank.posY = tank.posY - tank.velocidade * dt
     end
 
     if love.keyboard.isDown("down") then
-        nave.posY = nave.posY + nave.velocidade * dt
+        tank.posY = tank.posY + tank.velocidade * dt
     end
 
 
-
 end
-
 function love.draw()
     rot = angulo
-    love.graphics.draw(imgTank, nave.posX, nave.posY, 0, 1, 1, imgTank:getWidth()/2, imgTank:getHeight()/2)
+    love.graphics.draw(imgTank, tank.posX, tank.posY, 0, 1, 1, imgTank:getWidth()/2, imgTank:getHeight()/2)
 end
